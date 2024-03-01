1b524a809c96674f98fcdfe8c17158ecccd3ec14
Femas Akbar Faturrohim <111944157+femas66@users.noreply.github.com>
2024-03-01 15:09:24 +0700

    lurus tabel absen

--- Android/app/src/main/java/com/yudas1337/recognizeface/screens/EmployeeAttendanceActivity.kt
+++ Android/app/src/main/java/com/yudas1337/recognizeface/screens/EmployeeAttendanceActivity.kt
@@ -3,32 +3,36 @@ package com.yudas1337.recognizeface.screens
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import com.yudas1337.recognizeface.R
 import com.yudas1337.recognizeface.adapter.AttendanceAdapter
-import kotlinx.android.synthetic.main.activity_attendance_today.btn_bck
+import com.yudas1337.recognizeface.adapter.EmployeeAttendanceAdapter
 import kotlinx.android.synthetic.main.activity_attendance_today.recycler_view
 
-class AttendanceTodayActivity : AppCompatActivity() {
+class EmployeeAttendanceActivity : AppCompatActivity() {
     private var results: List<String> = listOf("SMKN 1 KEPANJEN", "SMKN 1 LUMAJANG", "SMK NEGERI 1 LUAMAJANG", "tester", "femas", "arip", "ibnu");
-    private var viewAdapter: AttendanceAdapter? = null
+    private var viewAdapter: EmployeeAttendanceAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_attendance_today)
+        setContentView(R.layout.activity_employee_attendance)
 
         val gridLayoutManager = GridLayoutManager(this,1)
         recycler_view!!.layoutManager = gridLayoutManager
-        viewAdapter = AttendanceAdapter(this, results)
+
+        viewAdapter = EmployeeAttendanceAdapter(this, results)
         recycler_view!!.adapter = viewAdapter
 
-        val btnEmployee = findViewById<LinearLayout>(R.id.buttonEmployee)
-        btnEmployee.setOnClickListener {
-            startActivity(Intent(this, EmployeeAttendanceActivity::class.java))
+        val btnStudent = findViewById<LinearLayout>(R.id.tabStudent);
+        btnStudent.setOnClickListener {
+            startActivity(Intent(this, AttendanceTodayActivity::class.java))
             finish()
         }
 
-        btn_bck.setOnClickListener {
+        val buttonBack = findViewById<Button>(R.id.btn_bck)
+
+        buttonBack.setOnClickListener {
             startActivity(Intent(this, MenuActivity::class.java))
             finish()
         }
