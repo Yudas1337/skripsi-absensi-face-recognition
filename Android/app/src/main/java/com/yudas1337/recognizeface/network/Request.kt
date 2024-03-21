b8892b4d9f78cc37a3ba184a691d17dab94546fe
frizehero <yudasmalabi@gmail.com>
2024-03-22 00:14:16 +0700

    remove unused import

--- Android/app/src/main/java/com/yudas1337/recognizeface/network/Request.kt
+++ Android/app/src/main/java/com/yudas1337/recognizeface/network/Request.kt
@@ -1,13 +1,10 @@
 package com.yudas1337.recognizeface.network
 
-import okhttp3.MultipartBody
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Multipart
 import retrofit2.http.POST
-import retrofit2.http.Part
 import retrofit2.http.Url
 
 interface Request {
