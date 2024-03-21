853f17b25c4d7b76f6be3b45e3c9e8fb7534efac
Yudas <yudasmalabi@gmail.com>
2024-03-21 19:11:01 +0700

    update search and laporan

--- Android/app/src/main/java/com/yudas1337/recognizeface/screens/AttendanceTodayActivity.kt
+++ Android/app/src/main/java/com/yudas1337/recognizeface/screens/AttendanceTodayActivity.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.database.getStringOrNull
 import androidx.recyclerview.widget.GridLayoutManager
@@ -14,6 +18,8 @@ import com.yudas1337.recognizeface.database.AttendanceData
 import com.yudas1337.recognizeface.database.DBHelper
 import kotlinx.android.synthetic.main.activity_attendance_today.btn_bck
 import kotlinx.android.synthetic.main.activity_attendance_today.recycler_view
+import kotlinx.android.synthetic.main.activity_attendance_today.searchButton
+import kotlinx.android.synthetic.main.activity_attendance_today.searchText
 
 class AttendanceTodayActivity : AppCompatActivity() {
     private var viewAdapter: AttendanceAdapter? = null
@@ -31,6 +37,23 @@ class AttendanceTodayActivity : AppCompatActivity() {
         btnEmployee.setOnClickListener {
             startActivity(Intent(this, EmployeeAttendanceActivity::class.java))
             finish()
+        }
+
+        searchButton.setOnClickListener{
+            Toast.makeText(this, "di klik", Toast.LENGTH_SHORT).show()
+        }
+
+        searchText.setOnEditorActionListener { _, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE ||
+                event != null && event.action == KeyEvent.ACTION_DOWN &&
+                event.keyCode == KeyEvent.KEYCODE_ENTER) {
+                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(searchText.windowToken, 0)
+
+                Toast.makeText(this, "Enter pressed", Toast.LENGTH_SHORT).show()
+                return@setOnEditorActionListener true
+            }
+            false
         }
 
         btn_bck.setOnClickListener {
